d45fe2bd7b63138211889f31cec6817ea9fb4662
Muhammedshifin <muhammedshifink4@gmail.com>
2024-10-22 07:25:21 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -664,11 +664,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
             alert = alert.replace("\\n", "\n").replace("\\t", "\t")
             await query.answer(alert, show_alert=True)
     if query.data.startswith("file"):
-        clicked = query.from_user.id
-        try:
-            typed = query.message.reply_to_message.from_user.id
-        except:
-            typed = query.from_user.id
+        clicked = 0
+        typed = 0
         ident, file_id = query.data.split("#")
         files_ = await get_file_details(file_id)
         if not files_:
